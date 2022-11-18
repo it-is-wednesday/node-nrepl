5d2a1f09c127cd1758cadc1d847c6c6ce9e6fe75
Maor Kadosh <maork@venn.city>
2022-11-18 12:00:22 +0100

    init
    only `clone` and `describe` implemented

--- index.ts
+++ index.ts
@@ -1,11 +1,41 @@
 import { createServer, Socket } from "net";
+import { randomUUID } from "crypto";
 import * as bencode from "bencode";
+import dayjs from "dayjs";
+
+function describe() {
+  const ops = {};
+  for (const op of Object.keys(opIndex)) {
+    // idk why all implementations return a map with empty values rather than a list
+    ops[op] = {};
+  }
+
+  return {
+    aux: {},
+    ops,
+  };
+}
+
+function clone() {
+  return {
+    "new-session": randomUUID(),
+    "time-stamp": dayjs().format("YYYY-MM-DD hh:mm:ss.SSS000000"),
+    status: ["done"],
+  };
+}
+
+const opIndex = {
+  clone,
+  describe,
+  // TODO catchall
+};
 
 const server = createServer((socket: Socket) => {
   socket.on("data", (data) => {
     const input = bencode.decode(data, "utf8");
     console.log(input);
-    socket.pipe(socket);
+    const resp = opIndex[input["op"]]();
+    socket.write(bencode.encode({ ...resp, id: input["id"] }));
   });
 });
 
