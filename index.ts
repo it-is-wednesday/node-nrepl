50c270bebdd92226f106b2c4daa666a892b0372f
Maor Kadosh <maork@venn.city>
2022-11-19 00:44:50 +0100

    add expressions-only eval

--- index.ts
+++ index.ts
@@ -3,10 +3,30 @@ import { randomUUID } from "crypto";
 import * as bencode from "bencode";
 import dayjs from "dayjs";
 
+type Operation = "clone" | "describe" | "eval";
+
+interface Message {
+  id: string;
+  session?: string;
+  op: Operation;
+}
+
+interface OpEval extends Message {
+  code: string;
+  ns: string;
+  op: "eval";
+}
+
+function eval_(input: OpEval) {
+  const evaldValue = Function(`"use strict";return (${input.code})`)();
+  const prettyPrinted = JSON.stringify(evaldValue, null, 2);
+  return [{ value: prettyPrinted }, { status: ["done"] }];
+}
+
 function describe() {
   const ops = {};
+  // fill ops map with empty maps as values, I'm not sure why that's the standard
   for (const op of Object.keys(opIndex)) {
-    // idk why all implementations return a map with empty values rather than a list
     ops[op] = {};
   }
 
@@ -24,18 +44,42 @@ function clone() {
   };
 }
 
-const opIndex = {
+type opFunc = (nreplClientInput: Message) => any | any[];
+
+const opIndex: { [idx: string]: opFunc } = {
   clone,
   describe,
+  eval: eval_,
   // TODO catchall
 };
 
 const server = createServer((socket: Socket) => {
-  socket.on("data", (data) => {
-    const input = bencode.decode(data, "utf8");
-    console.log(input);
-    const resp = opIndex[input["op"]]();
-    socket.write(bencode.encode({ ...resp, id: input["id"] }));
+  let sessionId: string;
+
+  socket.on("data", (socketData: Buffer) => {
+    const clientMsg = bencode.decode(socketData, "utf8");
+
+    let msgsToSend = opIndex[clientMsg["op"]](clientMsg);
+    if (!Array.isArray(msgsToSend)) {
+      msgsToSend = [msgsToSend];
+    }
+
+    for (let msg of msgsToSend) {
+      msg["id"] = clientMsg["id"];
+
+      if (msg["new-session"]) {
+        sessionId = msg["new-session"];
+      } else {
+        msg["session"] = sessionId;
+      }
+
+      console.log("input:");
+      console.log(clientMsg);
+      console.log("output:");
+      console.log(msg);
+
+      socket.write(bencode.encode(msg));
+    }
   });
 });
 
