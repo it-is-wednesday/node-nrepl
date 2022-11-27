0ec6beb110d225f1162b962509cf8517cfa76d6d
Maor Kadosh <maork@venn.city>
2022-11-27 02:28:56 +0200

    error handling (#1)

--- index.ts
+++ index.ts
@@ -63,19 +63,45 @@ const server = createServer((socket: Socket) => {
         send({ aux: {}, ops: operations });
         break;
       case "eval":
-        send({ value: nodeUtil.inspect(repl.evalCode(clientMsg.code)) });
-        send({ status: ["done"] });
+        sendEvalResultsOrErrors(repl.evalCode, send, clientMsg.code);
         break;
       case "load-file":
-        send({ value: nodeUtil.inspect(repl.evalCode(clientMsg.file)) });
-        send({ status: ["done"] });
+        sendEvalResultsOrErrors(repl.evalCode, send, clientMsg.file);
         break;
       default:
         console.error(`Unknown op: '${clientMsg["op"]}'`);
-        send({status: ["error", "unknown-op", "done"]});
+        send({ status: ["error", "unknown-op", "done"] });
     }
   });
 });
+
+/**
+ * OpEval abstraction
+ * Evaluate `code` using `evalFunc`, sending its result using `sendFunc`.
+ * If any errors were thrown, sends an "err" message and an "eval-error" one.
+ * Regardless, sends a "done" status message at the end.
+ */
+function sendEvalResultsOrErrors(
+  evalFunc: (code: string) => any,
+  sendFunc: (toSend: any) => boolean,
+  code: string
+) {
+  try {
+    sendFunc({ value: nodeUtil.inspect(evalFunc(code)) });
+  } catch (exception) {
+    // This is non-standard, but seems like clients expect it
+    sendFunc({ err: `${exception.toString()}\n` });
+
+    const elaborateError = `${exception.toString()}\n${exception.stack}`;
+    console.error(elaborateError);
+    sendFunc({
+      ex: elaborateError,
+      "root-ex": exception.name,
+      status: ["eval-error"],
+    });
+  }
+  sendFunc({ status: ["done"] });
+}
 
 /**
  * Returns a ts-node REPL allowing redeclarations
